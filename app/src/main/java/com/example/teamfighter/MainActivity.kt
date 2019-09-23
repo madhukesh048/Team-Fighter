d64f8807ac52f3bf46abee82f99f8d47b783c07d
Madhukesh <madhukesh04@gmail.com>
2019-09-23 17:29:31 +0530

    ui styling

--- app/src/main/java/com/example/teamfighter/MainActivity.kt
+++ app/src/main/java/com/example/teamfighter/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,7 +43,10 @@ class MainActivity : AppCompatActivity() {
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         timeLeftTextView = findViewById(R.id.timeLeftTextView)
 
-        tapMeButton.setOnClickListener{ view -> incrementScore()
+        tapMeButton.setOnClickListener{ view ->
+            val bounceAnimation = AnimationUtils.loadAnimation(this,R.anim.bounce)
+            view.startAnimation(bounceAnimation)
+            incrementScore()
         }
 
         if(savedInstanceState!= null){
@@ -49,6 +56,30 @@ class MainActivity : AppCompatActivity() {
         }else {
             resetGame()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.actionAbout){
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.aboutTitle,BuildConfig.VERSION_NAME)
+        var dialogMessage = getString(R.string.aboutMessage)
+
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+
     }
 
     override  fun onSaveInstanceState(outState:Bundle){
@@ -113,6 +144,9 @@ class MainActivity : AppCompatActivity() {
         score += 1
         val newScore = getString(R.string.yourScore,score)
         gameScoreTextView.text = newScore
+
+        val blinkAnimation = AnimationUtils.loadAnimation(this,R.anim.blink)
+        gameScoreTextView.startAnimation(blinkAnimation)
     }
 
     private fun startGame(){
